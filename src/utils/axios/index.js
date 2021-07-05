3f377fe4fe56ec5f2830f2383afff2e7b733e971
Mansour Almohsen <mansouralmohsen2018@gmail.com>
2021-07-05 14:18:02 +0300

    change URL

--- src/utils/axios/index.js
+++ src/utils/axios/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { initializeStore } from '@redux/store';
 
 const API = axios.create({
-    // baseURL: `http://157.175.95.127:3003/v1/`,
-    baseURL: `http://localhost:3003/v1/`,
+    baseURL: `http://157.175.95.127:3003/v1/`,
+    // baseURL: `http://localhost:3003/v1/`,
     timeout: 30000,
     headers: {
         Authorization: `Bearer ${initializeStore().getState().user?.token}`
