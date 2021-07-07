1069858ab3a312e01bfd346cf49f1dc1a012bb1f
mohamed <mohamedhassansulatan@gmail.com>
2021-07-07 19:13:23 +0300

    enhance-error-login

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,179 +1,190 @@
 // import PropTypes from 'prop-types';
 
-import { Col, ConfigProvider, Form, Image, Input, Row, Typography } from 'antd';
+import { Col, ConfigProvider, Form, Image, Input, Row, Typography } from "antd";
 
-import API from '@utils/axios';
-import CustomButton from '@src/components/CustomBtn';
-import LangChanger from '@src/components/LangToggle';
-import Link from 'next/link';
-import Placeholder from '@components/Placeholder';
-import PropTypes from 'prop-types';
-import authStyles from '@styles/Auth.module.scss';
-import authenticatedRoute from '@components/AuthenticatedRoute';
-import { setUser } from '@redux/actions/user';
-import toastr from 'toastr';
-import { useDispatch } from 'react-redux';
-import { useRouter } from 'next/router';
-import { useState } from 'react';
-import useTranslation from 'next-translate/useTranslation';
+import API from "@utils/axios";
+import CustomButton from "@src/components/CustomBtn";
+import LangChanger from "@src/components/LangToggle";
+import Link from "next/link";
+import Placeholder from "@components/Placeholder";
+import PropTypes from "prop-types";
+import authStyles from "@styles/Auth.module.scss";
+import authenticatedRoute from "@components/AuthenticatedRoute";
+import { setUser } from "@redux/actions/user";
+import toastr from "toastr";
+import { useDispatch } from "react-redux";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import useTranslation from "next-translate/useTranslation";
 
 const { Text } = Typography;
 
 const Login = ({ direction }) => {
-    const dispatch = useDispatch();
-    const { t } = useTranslation('login');
-    const router = useRouter();
-    const [loading, setLoading] = useState(false);
-    const requiredField = t('common:requiredInput');
+  const dispatch = useDispatch();
+  const { t } = useTranslation("login");
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const requiredField = t("common:requiredInput");
 
-    const onFinish = ({ email, password }) => {
-        setLoading(true);
-        API.post('auth/signin', {
-            email,
-            password
-        })
-            .then((res) => {
-                try {
-                    setLoading(false);
+  const onFinish = ({ email, password }) => {
+    setLoading(true);
+    API.post("auth/signin", {
+      email,
+      password,
+    })
+      .then((res) => {
+        try {
+          setLoading(false);
 
-                    if (res?.status === 201) {
-                        dispatch(setUser(res.data));
-                        // cookie.set('token', res.data.accessToken, { expires: 24 });
-                        fetch('/api/auth/login', {
-                            method: 'post',
-                            headers: {
-                                'Content-Type': 'application/json'
-                            },
-                            body: JSON.stringify({ token: res.data.accessToken })
-                        }).then(() => {
-                            router.push('/overview');
-                        });
-                    }
-                } catch (error) {
-                    toastr.error('something went wrong');
-                }
-            })
-            .catch((err) => {
-                if (err.response?.data?.message) {
-                    toastr.error(err.response.data?.message);
-                } else if (err.message) {
-                    toastr.error(err.message);
-                } else if (err.request) {
-                    toastr.error(err.request);
-                }
-                setLoading(false);
+          if (res?.status === 201) {
+            dispatch(setUser(res.data));
+            // cookie.set('token', res.data.accessToken, { expires: 24 });
+            fetch("/api/auth/login", {
+              method: "post",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ token: res.data.accessToken }),
+            }).then(() => {
+              router.push("/overview");
             });
-    };
-    // const onFinishFailed = (errorInfo) => {
-    //     toastr.warning('Something went wrong');
-    // };
-    return (
-        <Row>
-            <Col
-                xs={0}
-                md={12}
-                type="flex"
-                justify="center"
-                align="center"
-                className={authStyles.authLeftSide}>
-                <Placeholder />
+          }
+        } catch (error) {
+          toastr.error("something went wrong");
+        }
+      })
+      .catch((err) => {
+        alert(direction);
+        setLoading(false);
+        if (err.response?.data?.error?.message) {
+          return toastr.error(
+            err.response.data.error.message[direction == "ltr" ? "ar" : "en"]
+          );
+        }
+
+        if (err.response?.data?.message) {
+          toastr.error(err.response.data?.message);
+        } else if (err.message) {
+          toastr.error(err.message);
+        } else if (err.request) {
+          toastr.error(err.request);
+        }
+      });
+  };
+  // const onFinishFailed = (errorInfo) => {
+  //     toastr.warning('Something went wrong');
+  // };
+  return (
+    <Row>
+      <Col
+        xs={0}
+        md={12}
+        type="flex"
+        justify="center"
+        align="center"
+        className={authStyles.authLeftSide}
+      >
+        <Placeholder />
+      </Col>
+      <ConfigProvider direction={direction}>
+        <Col
+          xs={24}
+          md={12}
+          type="flex"
+          justify="center"
+          align="center"
+          direction="column"
+          className={authStyles.authRightSide}
+        >
+          <Row type="flex" justify="center" align="middle">
+            <LangChanger abs={true} />
+            <Col span={18}>
+              <Row justify="center">
+                <Image
+                  // preview={false}
+                  width={100}
+                  src="/assets/logo-dark-notext.png"
+                  className="logo-Login"
+                />
+              </Row>
+              <Row justify="space-around">
+                <p className="title-1 dark-blue">{t("login")}</p>
+              </Row>
+              <Form
+                name="basic"
+                className="form-container"
+                layout="vertical"
+                onFinish={onFinish}
+                // onFinishFailed={onFinishFailed}
+              >
+                <Row justify="space-around" align="middle">
+                  <Col span={23}>
+                    <Form.Item
+                      validateTrigger={"onBlur"}
+                      label={t("email")}
+                      name="email"
+                      className="dark-blue mb-1"
+                      rules={[
+                        {
+                          required: true,
+                          type: "email",
+                          message: t("emailError"),
+                        },
+                      ]}
+                    >
+                      <Input className={authStyles.input} />
+                    </Form.Item>
+                  </Col>
+
+                  <Col span={23}>
+                    <Form.Item
+                      label={t("password")}
+                      className="mb-1"
+                      name="password"
+                      rules={[
+                        {
+                          required: true,
+                          message: requiredField,
+                        },
+                      ]}
+                    >
+                      <Input.Password className={authStyles.input} />
+                    </Form.Item>
+                  </Col>
+                </Row>
+
+                <Row justify="center" align="middle">
+                  <Col span={24}>
+                    <Form.Item>
+                      <CustomButton
+                        htmlType="submit"
+                        text={t("login")}
+                        className={`${authStyles.btnRegister} btn-text`}
+                        loading={loading}
+                      />
+                    </Form.Item>
+                  </Col>
+                </Row>
+                <Text type="secondary" className="gothic">
+                  {t("newHere")}
+                  &nbsp;
+                  <Link className="blue pointer" href="/signup">
+                    <a>
+                      {t("register")}&nbsp;
+                      {t("now")}
+                    </a>
+                  </Link>
+                </Text>
+              </Form>
             </Col>
-            <ConfigProvider direction={direction}>
-                <Col
-                    xs={24}
-                    md={12}
-                    type="flex"
-                    justify="center"
-                    align="center"
-                    direction="column"
-                    className={authStyles.authRightSide}>
-                    <Row type="flex" justify="center" align="middle">
-                        <LangChanger abs={true} />
-                        <Col span={18}>
-                            <Row justify="center">
-                                <Image
-                                    // preview={false}
-                                    width={100}
-                                    src="/assets/logo-dark-notext.png"
-                                    className="logo-Login"
-                                />
-                            </Row>
-                            <Row justify="space-around">
-                                <p className="title-1 dark-blue">{t('login')}</p>
-                            </Row>
-                            <Form
-                                name="basic"
-                                className="form-container"
-                                layout="vertical"
-                                onFinish={onFinish}
-                                // onFinishFailed={onFinishFailed}
-                            >
-                                <Row justify="space-around" align="middle">
-                                    <Col span={23}>
-                                        <Form.Item
-                                            validateTrigger={'onBlur'}
-                                            label={t('email')}
-                                            name="email"
-                                            className="dark-blue mb-1"
-                                            rules={[
-                                                {
-                                                    required: true,
-                                                    type: 'email',
-                                                    message: t('emailError')
-                                                }
-                                            ]}>
-                                            <Input className={authStyles.input} />
-                                        </Form.Item>
-                                    </Col>
-
-                                    <Col span={23}>
-                                        <Form.Item
-                                            label={t('password')}
-                                            className="mb-1"
-                                            name="password"
-                                            rules={[
-                                                {
-                                                    required: true,
-                                                    message: requiredField
-                                                }
-                                            ]}>
-                                            <Input.Password className={authStyles.input} />
-                                        </Form.Item>
-                                    </Col>
-                                </Row>
-
-                                <Row justify="center" align="middle">
-                                    <Col span={24}>
-                                        <Form.Item>
-                                            <CustomButton
-                                                htmlType="submit"
-                                                text={t('login')}
-                                                className={`${authStyles.btnRegister} btn-text`}
-                                                loading={loading}
-                                            />
-                                        </Form.Item>
-                                    </Col>
-                                </Row>
-                                <Text type="secondary" className="gothic">
-                                    {t('newHere')}
-                                    &nbsp;
-                                    <Link className="blue pointer" href="/signup">
-                                        <a>
-                                            {t('register')}&nbsp;
-                                            {t('now')}
-                                        </a>
-                                    </Link>
-                                </Text>
-                            </Form>
-                        </Col>
-                    </Row>
-                </Col>
-            </ConfigProvider>
-        </Row>
-    );
+          </Row>
+        </Col>
+      </ConfigProvider>
+    </Row>
+  );
 };
 Login.propTypes = {
-    direction: PropTypes.string.isRequired
+  direction: PropTypes.string.isRequired,
 };
 // export default Login;
 export default authenticatedRoute(Login);
