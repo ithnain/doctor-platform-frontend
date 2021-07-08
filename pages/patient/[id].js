fb10f4257de7d4287a436df26f13db15b5684414
mohamed <mohamedhassansulatan@gmail.com>
2021-07-08 20:56:22 +0300

    add-props-types=resuable-component

--- pages/patient/[id].js
+++ pages/patient/[id].js
@@ -1,91 +1,92 @@
-import React from "react";
-import patienProfileSyle from "@styles/PatientProfile.module.scss";
-import useTranslation from "next-translate/useTranslation";
-import authenticatedRoute from "@components/AuthenticatedRoute";
-import SliderLayout from "@components/Layout";
-import PropTypes from "prop-types";
+import React from 'react';
+import patienProfileSyle from '@styles/PatientProfile.module.scss';
+import useTranslation from 'next-translate/useTranslation';
+import authenticatedRoute from '@components/AuthenticatedRoute';
+import SliderLayout from '@components/Layout';
+import PropTypes from 'prop-types';
 
-import { useRouter } from "next/router";
-import API from "@utils/axios";
+import { useRouter } from 'next/router';
+import API from '@utils/axios';
 
 import {
-  UserCardInfo,
-  DbCarInfo,
-  NotesCard,
-  AvatarWithEdit,
-  DividerLine,
-} from "@components/PatientProfile";
+    UserCardInfo,
+    DbCarInfo,
+    NotesCard,
+    AvatarWithEdit,
+    DividerLine
+} from '@components/PatientProfile';
 
 const PatientProfile = ({ patient, direction }) => {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  const router = useRouter();
+    const router = useRouter();
 
-  if (router.isFallback) {
-    return <div>{t("Loding")}</div>;
-  }
-  if (!patient) return <h1>{t("NotFOund")}</h1>;
-  return (
-    <div>
-      <SliderLayout
-        title={"PtientProfile"}
-        keywords={"PtientProfile"}
-        description={"this is patinet profile"}
-        active={`/patient`}
-      >
-        <div
-          style={{
-            direction,
-          }}
-        >
-          <h6 className={patienProfileSyle.header}>{`${t("patient")} ${t(
-            "profile"
-          )}`}</h6>
+    if (router.isFallback) {
+        return <div>{t('Loding')}</div>;
+    }
+    if (!patient) return <h1>{t('NotFOund')}</h1>;
+    return (
+        <div>
+            <SliderLayout
+                title={'PtientProfile'}
+                keywords={'PtientProfile'}
+                description={'this is patinet profile'}
+                active={`/patient`}>
+                <div
+                    style={{
+                        direction
+                    }}>
+                    <h6 className={patienProfileSyle.header}>{`${t('patient')} ${t(
+                        'profile'
+                    )}`}</h6>
 
-          <div>
-            <AvatarWithEdit name={patient.name} />
-            <UserCardInfo
-              t={t}
-              age={patient.age}
-              phone_number={patient.phone_number}
-              city={patient.city}
-            />
-            <DividerLine />
-            <DbCarInfo
-              t={t}
-              ISF={patient.ISF}
-              sliding_scale={patient.sliding_scale}
-              is_other_health_issues={patient.s_other_health_issues}
-              I_C={patient.I_C}
-              health_issues={patient.health_issues}
-            />
-            <DividerLine />
-            <NotesCard note={patient.note} t={t} />
-          </div>
+                    <div>
+                        <AvatarWithEdit name={patient.name} />
+                        <UserCardInfo
+                            t={t}
+                            age={patient.age}
+                            phone_number={patient.phone_number}
+                            city={patient.city}
+                        />
+                        <DividerLine />
+                        <DbCarInfo
+                            t={t}
+                            ISF={patient.ISF}
+                            sliding_scale={patient.sliding_scale}
+                            is_other_health_issues={patient.s_other_health_issues}
+                            I_C={patient.I_C}
+                            health_issues={patient.health_issues}
+                        />
+                        <DividerLine />
+                        <NotesCard note={patient.note} t={t} />
+                    </div>
+                </div>
+            </SliderLayout>
         </div>
-      </SliderLayout>
-    </div>
-  );
+    );
 };
 
 export async function getServerSideProps({ params, req }) {
-  let patient = null;
-  try {
-    const res = await API.get(`patient/patient?id=${params.id}`, {
-      headers: {
-        Authorization: `Bearer ${req.cookies.token}`,
-      },
-    });
-    patient = res.data;
-  } catch (error) {}
+    let patient;
+    try {
+        const res = await API.get(`patient/patient?id=${params.id}`, {
+            headers: {
+                Authorization: `Bearer ${req.cookies.token}`
+            }
+        });
+        patient = res.data;
+    } catch (error) {
+        patient = null;
+    }
 
-  return { props: { patient } };
+    return { props: { patient } };
 }
 
-PatientProfile.prototype = {
-  direction: PropTypes.string.isRequired,
-  doctors: PropTypes.object.isRequired,
+PatientProfile.propTypes = {
+    direction: PropTypes.string.isRequired,
+    patient: PropTypes.object.isRequired
 };
+
 export default authenticatedRoute(PatientProfile, {
-  pathAfterFailure: "/login",
+    pathAfterFailure: '/login'
 });
--- src/components/PatientProfile/index.js
+++ src/components/PatientProfile/index.js
@@ -1,123 +1,124 @@
-import React from "react";
-import patienProfileSyle from "@styles/PatientProfile.module.scss";
-import { Col } from "antd";
-import { PersonIcon, GenderIcon, EditIcon } from "@utils/svg/patientProfile";
+import React from 'react';
+import patienProfileSyle from '@styles/PatientProfile.module.scss';
+import PropTypes from 'prop-types';
+
+import { Col } from 'antd';
+import { PersonIcon, GenderIcon, EditIcon } from '@utils/svg/patientProfile';
 
 export const UserCardInfo = ({ age, phone_number, city, t }) => (
-  <div className={patienProfileSyle.UserCardInfoWrapper}>
-    <h6 className={patienProfileSyle.labelWidht}>{`${t("patinet")} ${t(
-      "information"
-    )}`}</h6>
+    <div className={patienProfileSyle.UserCardInfoWrapper}>
+        <h6 className={patienProfileSyle.labelWidht}>{`${t('patinet')} ${t('information')}`}</h6>
 
-    <div className={patienProfileSyle.userinfoCard}>
-      <div style={{ flex: 1 }}>
-        <div className={patienProfileSyle.columOneTwoWrapper}>
-          <div className={patienProfileSyle.columOneTwoWrapperColumOne}>
-            <RenderInfoText title={t("age")} info={age} />
-            <RenderInfoText title={t("phone")} info={phone_number} />
-            <RenderInfoText title={t("city")} info={city} />
-          </div>
-          <div className={patienProfileSyle.columOneTwoWrapperColumTwo}>
-            <RenderInfoText title={t("CoveredTopics")} info="xxx,xxxxx,xxxxx" />
-          </div>
-          <div />
+        <div className={patienProfileSyle.userinfoCard}>
+            <div className={patienProfileSyle.flex1}>
+                <div className={patienProfileSyle.columOneTwoWrapper}>
+                    <div className={patienProfileSyle.columOneTwoWrapperColumOne}>
+                        <RenderInfoText title={t('age')} info={age} />
+                        <RenderInfoText title={t('phone')} info={phone_number} />
+                        <RenderInfoText title={t('city')} info={city} />
+                    </div>
+                    <div className={patienProfileSyle.columOneTwoWrapperColumTwo}>
+                        <RenderInfoText title={t('CoveredTopics')} info="xxx,xxxxx,xxxxx" />
+                    </div>
+                    <div />
+                </div>
+            </div>
+            <a className={patienProfileSyle.basicButton}>{t('viewHistory')} </a>
         </div>
-      </div>
-      <a className={patienProfileSyle.basicButton}>{t("viewHistory")} </a>
     </div>
-  </div>
 );
-export const DbCarInfo = ({
-  t,
-  ISF,
-  sliding_scale,
-  is_other_health_issues,
-  I_C,
-  health_issues,
-}) => (
-  <div className={patienProfileSyle.diabetsInformationWrapper}>
-    <h6 className={patienProfileSyle.labelWidht}>{`${t("Diabetes")} ${t(
-      "information"
-    )}`}</h6>
-    <div className={patienProfileSyle.diabetsInformationWrapperOne}>
-      <div style={{ flex: 1 }}>
-        <div className={patienProfileSyle.columOneTwoWrapper}>
-          <div className={patienProfileSyle.columOneTwoWrapperColumOne}>
-            <RenderInfoText title={t("patientType")} info="I" />
-            <RenderInfoText title={t("patientType")} info="xxxxxxxxxxxx" />
-            <RenderInfoText title={t("ResponsableOfPatient")} info="Paris" />
-            <RenderInfoText title={t("PatientIsOn")} info="Paris" />
-            <RenderInfoText title={t("ISF")} info={ISF} />
-            <RenderInfoText title={t("IC")} info={I_C} />
-            <RenderInfoText title={t("SlidingSclae")} info={sliding_scale} />
-            <RenderInfoText
-              title={t("CriticalHealthIssue")}
-              info={health_issues[0]}
-            />
-          </div>
-          <div className={patienProfileSyle.columOneTwoWrapperColumTwo}>
-            <RenderInfoText
-              title={t("MedicalConditions")}
-              info="xxx,xxxxx,xxxxx"
-            />
-          </div>
-          <div />
+UserCardInfo.propTypes = {
+    age: PropTypes.string,
+    phone_number: PropTypes.string,
+    city: PropTypes.string,
+    t: PropTypes.func
+};
+export const DbCarInfo = ({ t, ISF, sliding_scale, I_C, health_issues }) => (
+    <div className={patienProfileSyle.diabetsInformationWrapper}>
+        <h6 className={patienProfileSyle.labelWidht}>{`${t('Diabetes')} ${t('information')}`}</h6>
+        <div className={patienProfileSyle.diabetsInformationWrapperOne}>
+            <div className={patienProfileSyle.flex1}>
+                <div className={patienProfileSyle.columOneTwoWrapper}>
+                    <div className={patienProfileSyle.columOneTwoWrapperColumOne}>
+                        <RenderInfoText title={t('patientType')} info="I" />
+                        <RenderInfoText title={t('patientType')} info="xxxxxxxxxxxx" />
+                        <RenderInfoText title={t('ResponsableOfPatient')} info="Paris" />
+                        <RenderInfoText title={t('PatientIsOn')} info="Paris" />
+                        <RenderInfoText title={t('ISF')} info={ISF} />
+                        <RenderInfoText title={t('IC')} info={I_C} />
+                        <RenderInfoText title={t('SlidingSclae')} info={sliding_scale} />
+                        <RenderInfoText title={t('CriticalHealthIssue')} info={health_issues[0]} />
+                    </div>
+                    <div className={patienProfileSyle.columOneTwoWrapperColumTwo}>
+                        <RenderInfoText title={t('MedicalConditions')} info="xxx,xxxxx,xxxxx" />
+                    </div>
+                    <div />
+                </div>
+            </div>
+            <a className={patienProfileSyle.basicButton}>{t('viewHistory')} </a>
         </div>
-      </div>
-      <a className={patienProfileSyle.basicButton}>{t("viewHistory")} </a>
     </div>
-  </div>
 );
+DbCarInfo.propTypes = {
+    t: PropTypes.func,
+    ISF: PropTypes.string,
+    sliding_scale: PropTypes.string,
+    is_other_health_issues: PropTypes.bool,
+    I_C: PropTypes.string,
+    health_issues: PropTypes.string
+};
 
 export const NotesCard = ({ note, t }) => (
-  <div className={patienProfileSyle.NotesCardWrapper}>
-    <Col>
-      <h6 className={patienProfileSyle.labelWidht}>{t("Notes")}</h6>
-    </Col>
-    <div className={patienProfileSyle.NotesCardWrapperInformation}>
-      <div className={patienProfileSyle.NotesCardWrapperInformationNote}>
-        <h6 className={patienProfileSyle.infoTextvalue}>
-          {t("DoctorRecommendationNotes")}
-        </h6>
-        <p className={patienProfileSyle.infoTextvalueValue}>{note}</p>
-      </div>
-      <div
-        className={
-          patienProfileSyle.NotesCardWrapperInformationViewHistoyWrapper
-        }
-      >
-        <a
-          className={
-            patienProfileSyle.NotesCardWrapperInformationViewHistoyWrapperBtn
-          }
-        >
-          {t("ViewAllNotes")}
-        </a>
-        <a className={patienProfileSyle.basicButtonBlue}>{t("EditNotes")}</a>
-      </div>
+    <div className={patienProfileSyle.NotesCardWrapper}>
+        <Col>
+            <h6 className={patienProfileSyle.labelWidht}>{t('Notes')}</h6>
+        </Col>
+        <div className={patienProfileSyle.NotesCardWrapperInformation}>
+            <div className={patienProfileSyle.NotesCardWrapperInformationNote}>
+                <h6 className={patienProfileSyle.infoTextvalue}>
+                    {t('DoctorRecommendationNotes')}
+                </h6>
+                <p className={patienProfileSyle.infoTextvalueValue}>{note}</p>
+            </div>
+            <div className={patienProfileSyle.NotesCardWrapperInformationViewHistoyWrapper}>
+                <a className={patienProfileSyle.NotesCardWrapperInformationViewHistoyWrapperBtn}>
+                    {t('ViewAllNotes')}
+                </a>
+                <a className={patienProfileSyle.basicButtonBlue}>{t('EditNotes')}</a>
+            </div>
+        </div>
     </div>
-  </div>
 );
+NotesCard.propTypes = {
+    note: PropTypes.string,
+    t: PropTypes.func
+};
 
 export const AvatarWithEdit = ({ name }) => (
-  <div className={patienProfileSyle.avatarWrapper}>
-    <div className={patienProfileSyle.avatarWrapperIconAndName}>
-      <div className={patienProfileSyle.personIconWrapper}>
-        <PersonIcon />
-      </div>
-      <h3 className={patienProfileSyle.avatarWrapperIconAndNameAvatarName}>
-        {name}
-      </h3>
-      <GenderIcon />
+    <div className={patienProfileSyle.avatarWrapper}>
+        <div className={patienProfileSyle.avatarWrapperIconAndName}>
+            <div className={patienProfileSyle.personIconWrapper}>
+                <PersonIcon />
+            </div>
+            <h3 className={patienProfileSyle.avatarWrapperIconAndNameAvatarName}>{name}</h3>
+            <GenderIcon />
+        </div>
+        <EditIcon />
     </div>
-    <EditIcon />
-  </div>
 );
+AvatarWithEdit.propTypes = {
+    name: PropTypes.string
+};
 
-export const RenderInfoText = ({ title, info }) => (
-  <h6 className={patienProfileSyle.infoTexthead}>
-    <b className={patienProfileSyle.infoTextvalue}>{title} :</b>{" "}
-    <b className={patienProfileSyle.infoTextvalueValue}> {info}</b>
-  </h6>
+const RenderInfoText = ({ title, info }) => (
+    <h6 className={patienProfileSyle.infoTexthead}>
+        <b className={patienProfileSyle.infoTextvalue}>{title} :</b>{' '}
+        <b className={patienProfileSyle.infoTextvalueValue}> {info}</b>
+    </h6>
 );
+RenderInfoText.propTypes = {
+    title: PropTypes.string,
+    info: PropTypes.string
+};
+
 export const DividerLine = () => <div className={patienProfileSyle.dvider} />;
