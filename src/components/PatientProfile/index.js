57b580a9dbc9313f57be67aa1bf5cd9a0349e109
Mohamed Fawzy <mohamed.f.elmorsi@gmail.com>
2021-12-28 14:09:13 +0200

    fix edit

--- src/components/PatientProfile/index.js
+++ src/components/PatientProfile/index.js
@@ -28,7 +28,7 @@ export const UserCardInfo = ({ age, phone_number, city, t }) => (
     </div>
 );
 UserCardInfo.propTypes = {
-    age: PropTypes.string,
+    age: PropTypes.number,
     phone_number: PropTypes.string,
     city: PropTypes.string,
     t: PropTypes.func
@@ -44,10 +44,10 @@ export const DbCarInfo = ({ t, ISF, sliding_scale, I_C, health_issues }) => (
                         <RenderInfoText title={t('patientType')} info="xxxxxxxxxxxx" />
                         <RenderInfoText title={t('ResponsableOfPatient')} info="Paris" />
                         <RenderInfoText title={t('PatientIsOn')} info="Paris" />
-                        <RenderInfoText title={t('ISF')} info={ISF} />
-                        <RenderInfoText title={t('IC')} info={I_C} />
-                        <RenderInfoText title={t('SlidingSclae')} info={sliding_scale} />
-                        <RenderInfoText title={t('CriticalHealthIssue')} info={health_issues[0]} />
+                        {/* <RenderInfoText title={t('ISF')} info={ISF} /> */}
+                        {/* <RenderInfoText title={t('IC')} info={I_C} /> */}
+                        {/* <RenderInfoText title={t('SlidingSclae')} info={sliding_scale} /> */}
+                        {/* <RenderInfoText title={t('CriticalHealthIssue')} info={health_issues[0]} /> */}
                     </div>
                     <div className={patienProfileSyle.columOneTwoWrapperColumTwo}>
                         <RenderInfoText title={t('MedicalConditions')} info="xxx,xxxxx,xxxxx" />
@@ -112,7 +112,7 @@ AvatarWithEdit.propTypes = {
 
 const RenderInfoText = ({ title, info }) => (
     <h6 className={patienProfileSyle.infoTexthead}>
-        <b className={patienProfileSyle.infoTextvalue}>{title} :</b>{' '}
+        <b className={patienProfileSyle.infoTextvalue}>{title} : </b>
         <b className={patienProfileSyle.infoTextvalueValue}> {info}</b>
     </h6>
 );
