3d215ba40ec4e306e4d6d260009d55c3004266a8
Wael A <waelvilla@gmail.com>
2022-02-12 18:05:44 +0300

    changed dev url to ECS server

--- src/utils/axios/index.js
+++ src/utils/axios/index.js
@@ -4,9 +4,10 @@ const API = axios.create({
     baseURL:
         process.env.NEXT_PUBLIC_APP_ENV === 'production'
             ? 'https://doctorsapi.ithnain.com/v1/'
-            : 'http://doctor-api-load-balancer-1831372393.me-south-1.elb.amazonaws.com:3003/v1/',
+            : 'https://doctorsapi.ithnain.com/v1/',
     timeout: 30000
 });
+        // : 'http://doctor-api-load-balancer-1831372393.me-south-1.elb.amazonaws.com:3003/v1/',
 
 API.interceptors.request.use(
     (config) => {
