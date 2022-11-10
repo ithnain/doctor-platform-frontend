4870e7d095adfb4a8ee3a246392ca6c37689c5c9
Moezzain <moezzain94@gmail.com>
2022-11-10 11:28:54 +0300

    empty commit

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -5,7 +5,7 @@ export default (req, res) => {
         'Set-Cookie',
         cookie.serialize('token', req.body.token, {
             httpOnly: true,
-            secure: process.env.APP_ENV !== 'development',
+            secure: process.env.APP_ENV !== 'development', 
             maxAge: 10 * 365 * 24 * 60 * 60,
             sameSite: 'strict',
             path: '/'
